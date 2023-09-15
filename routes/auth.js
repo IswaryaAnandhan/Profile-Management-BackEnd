9f75dff6b19e71dc1d78ee3f14815c0222392b2f
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2023-09-15 13:07:04 +0530

    Add change in auth

--- routes/auth.js
+++ routes/auth.js
@@ -37,7 +37,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
     const token = jwt.sign({ userId: user._id }, process.env.secretKey, { expiresIn: '1h' });
-    res.status(200).json({ token, userId: user._id });
+    res.status(200).json({ message: "Success",token, userId: user._id });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
